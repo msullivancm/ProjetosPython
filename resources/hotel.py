6eadbdcdeade448b5a6c40e17c1e3cc93b008e67
msullivancm <marcus.sullivan@gmail.com>
2022-11-29 16:57:57 -0300

    Criação do CRUD em memória

---
+++ resources/hotel.py
@@ -0,0 +1,77 @@
+from flask_restful import Resource, reqparse
+from models.hotel import *
+
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome': 'Alpha Hotel', 
+        'estrelas': 4.3,
+        'diaria': 420.34,
+        'cidade': 'Rio de Janeiro'
+    },
+    {
+        'hotel_id': 'bravo',
+        'nome': 'Bravo Hotel', 
+        'estrelas': 4.4,
+        'diaria': 380.90,
+        'cidade': 'Santa Catarina'
+    },
+    {
+        'hotel_id': 'charlie',
+        'nome': 'Charlie Hotel', 
+        'estrelas': 3.9,
+        'diaria': 320.20,
+        'cidade': 'Santa Catarina'
+    }    
+]
+
+class Hoteis(Resource):
+    def get(self):
+        return hoteis
+    
+class Hotel(Resource):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+        
+    def find_hotel(hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return None
+    #esta é uma função da classe, por isso não passa o self. 
+    # Se passasse o self procuraria só no registro atual e não no conjunto de registros da classe.
+    
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
+        return {'message': 'Hotel not found.'}, 404 #not found
+        
+    def post(self, hotel_id):
+        dados = Hotel.argumentos.parse_args()
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
+        
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200 #Sucesso
+    
+    def put(self, hotel_id):
+        dados = Hotel.argumentos.parse_args()
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
+
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel: 
+            hotel.update(novo_hotel)
+            return novo_hotel, 200 #ok atualizado
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201 #criado
+    
+    def delete(self, hotel_id):
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id']!= hotel_id]
+        return {'message': 'Hotel deleted.'}
+        
