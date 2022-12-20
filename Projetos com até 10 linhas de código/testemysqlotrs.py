ff3318a25eff63d28a1a49eba041ec9fef930a0c
msullivancm <marcus.sullivan@gmail.com>
2022-12-20 14:57:30 -0300

    correção da sintaxe do certificado ssl

--- Projetos com até 10 linhas de código/testemysqlotrs.py
+++ Projetos com até 10 linhas de código/testemysqlotrs.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 import pandas as pd
 
-db_connection_str = 'mysql+pymysql://frpro:senha@10.27.0.253/otrs'
-db_connection = create_engine(db_connection_str)
-#db_connection_str = 'mysql+pymysql://ferroport_ro:senha@ferroport.managed-otrs.com/otrs'
-#db_ssl_args = {'ssl_ca': 'C:/OTRS/ferroport-ca-cert.pem'}
-#db_connection = create_engine(db_connection_str, connect_args=db_ssl_args)
+db_connection_str = 'mysql+pymysql://ferroport_ro:senha@ferroport.managed-otrs.com/otrs'
+db_ssl_args = {'ssl_ca': 'C:/OTRS/ferroport-ca-cert.pem',
+               'ssl_key': 'C:/OTRS/ferroport-client-key.pem',
+               'ssl_cert': 'C:/OTRS/ferroport-client-cert.pem'}
+db_connection = create_engine(db_connection_str, connect_args=db_ssl_args)
 
 df = pd.read_sql('SELECT * FROM information_schema.tables', con=db_connection)
 
